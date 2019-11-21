8d55c66a51cecae40f6bafe27c1abf9a6860dd24
Markus Schwarz <mschwarz@black-app-solutions.com>
2019-11-21 12:17:51 +0100

    Improved JavaDoc

--- pdf-filter-cae/src/main/java/de/provocon/coremedia/cms/view/PdfFilterConfiguration.java
+++ pdf-filter-cae/src/main/java/de/provocon/coremedia/cms/view/PdfFilterConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * The entry point of our module, referenced by META-INF/spring.factories.
+ *
+ * The component scan below  takes care of instantiating {@link PdfCreationFilter}
+ *
  * @author Markus Schwarz
  */
 @Configuration
